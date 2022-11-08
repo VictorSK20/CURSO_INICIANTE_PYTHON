da4b90d0288689e2cd78318226c9c30380efe130
VictorSK20 <116979733+VictorSK20@users.noreply.github.com>
2022-11-08 15:57:34 -0400

    Delete Aula04_Desafio01.py

--- Aula04_Desafio01.py
+++
@@ -1,9 +0,0 @@
-print('LISTA DE CADASTRO'),
-
-nome = input(print('Digite seu nome:')),
-idade = input(print('Digite sua idade:')),
-peso = input(print('Digite seu peso:')),
-
-print('Usuario cadastrado'),
-print('Dados do usuario'),
-print('Nome:',nome,'idade:',idade,'peso',peso)
