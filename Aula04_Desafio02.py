9234c35f026beb31ee290a1ecf42058de5468424
VictorSK20 <116979733+VictorSK20@users.noreply.github.com>
2022-11-08 15:57:49 -0400

    Delete Aula04_Desafio02.py

--- Aula04_Desafio02.py
+++
@@ -1,9 +0,0 @@
-print('Cadastra Data de Nascimento'),
-print(' '),
-dia = input('Qual seu dia de Nascimento: '),
-mes = input('Qual seu mes de Nascimento: '),
-ano = input('Qual seu ano de Nascimento: '),
-print(' '),
-print('Cadastro realizado com sucesso'),
-print(' '),
-print('Data cadastrada:', dia, mes, ano)
