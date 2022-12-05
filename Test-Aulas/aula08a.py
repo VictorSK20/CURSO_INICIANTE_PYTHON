99ebcf34234bcc7a90c6efd491139a6b31b6ca22
VictorSK20 <116979733+VictorSK20@users.noreply.github.com>
2022-12-05 11:56:42 -0400

    Aula sobre o comando Import
    nessa aula é mostrado o comando import como o mesmo funciona

---
+++ Test-Aulas/aula08a.py
@@ -0,0 +1,15 @@
+# Aula sobre o comando import
+
+'''import math
+num = int(input('Digite um numero: '))
+raiz = math.fabs
+raiz2 = num ** 0.5
+print(f" a raiz quadrada do numero {num} = {raiz}")
+'''
+'''import random 
+num = int(input('Informe um numero: '))
+#total = num + random.random
+print(f'{num} + {random.random():.0f} = {num + random.random():.0f}')
+'''
+
+import
