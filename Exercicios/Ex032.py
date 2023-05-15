6dc6bc40d6757d48a5256ca9a9f726de93a2ab01
VictorSK20 <116979733+VictorSK20@users.noreply.github.com>
2023-05-15 15:07:56 -0400

    Criando um programa que ler um ano e informe se é Bissexto ou não
    Criando um programa que ler um ano e informe se é Bissexto ou não

--- Exercicios/Ex032.py
+++ Exercicios/Ex032.py
@@ -1,5 +1,20 @@
 # Crie um programa que leia um ano e informe se é Bissexto ou não
 
-ano = input('Informe o ano: ')[:4]
+# Adicionando uma informação na variavel ano
+ano = (input('Informe um ano e descubra se ele é Bissexto ou não: '))
 
-print(ano)
+# nessa parte o programa verificara se a variavel ano tem o tamanho diferente de 4 caracteres, se caso sim, o programa vai retorna uma mensagem de aviso
+if len(ano) != 4:
+    print('Favor digitar um ano válido com 4 digitos númericos')
+
+# Verificando se a variavel ano começa com 0, se caso sim, o programa vai retorna outro aviso
+elif ano[0] == '0':
+    print('Favor evite usa o digito zero na frente do ano')
+
+# O programa vai executar o calculo para verifica se a variavel ano é bissexto ou não e imprimir a mensagem na tela
+else:
+    ano = int(ano)
+    if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+        print(f'O ano de {ano} é BISSEXTO')
+    else:
+        print(f'O ano de {ano} não é BISSEXTO')
