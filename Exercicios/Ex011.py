bf9ed075a500e65d4bbb89c3c0ef0d61afa94816
VictorSK20 <116979733+VictorSK20@users.noreply.github.com>
2023-08-01 15:37:55 -0400

    Exercicios para da continuidade

--- Exercicios/Ex011.py
+++ Exercicios/Ex011.py
@@ -1,10 +1,12 @@
-print('Calculo da quantidade de tinta para uma parede')
+print('Calculo da quantidade de tinta para uma parede: ')
 print(' ')
 
 altura_prd = float(input('Informe a altura da parede em metros: '))
 largura_prd = float(input('Informe a largura da parede em metros: '))
+
 area = altura_prd * largura_prd
 tinta = area/2
-print('')
-print('Sua parede é {}x{} e sua area é de {}m²\nA quantidade de tinta necessaria para pinta a parede é de: {:.1f}L'.format(largura_prd, altura_prd, area, tinta))
 
+print('')
+print(f'Sua parede é \033[1;4;32m{largura_prd}\033[m x \033[1;4;32m{altura_prd}\033[0m e sua area é de \033[1;4;32m{area}m²\033[0m'
+      f'\nA quantidade de tinta necessaria para pinta a parede é de: \033[1;4;32m{tinta:.1f}L\033[m')
