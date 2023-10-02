ffb2aa917a5300107e7d7bc85babb9923d69ac41
VictorSK20 <116979733+VictorSK20@users.noreply.github.com>
2023-10-02 15:22:47 -0400

    testa

--- Exercicios/Ex031.py
+++ Exercicios/Ex031.py
@@ -1,7 +1,15 @@
 # Desenvolva um programa que pergunte a distancia de uma viagem em km. Calcule o preço da viagem
 # cobrando R$0,50 por Km para viagem de até 200Km e R$0,45 para viagens mais longas
 
-import math
+from colorama import init
+
+init(autoreset=True)
+
+# Amarelo
+cor1 = '\033[38;2;255;192;0m'
+
+# Laranja
+cor2 = '\033[38;2;255;128;0m'
 
 passagem = float(input('Qual será a distância da viagem\n'))
 preco1 = 0.50
@@ -9,7 +17,10 @@ preco2 = 0.45
 
 if passagem <= 200.00:
     total = preco1 * passagem
-    print(f'O valor da viagem esta a {total:.2f}R$')
+    print(f'{cor1}O valor da viagem esta a {total:.2f}R$\n\n')
 else:
     total = preco2 * passagem
-    print(f'O valor da viagem esta a {total:.2f}R$')
+    print(f'{cor2}O valor da viagem esta a {total:.2f}R$\n\n')
+
+print(f'{cor1}Viagens de até 200Km é cobrado R$0,50 por cada Kilometro \n'
+      f'{cor2}Viagens acima de 20Km é cobrado R$0,45 por cada Kilometro')
--- Exercicios/Ex035.py
+++ Exercicios/Ex035.py
@@ -1,26 +1,46 @@
 # Crie um programa que leia o comprimento de 3 retas e informe se essas retas podem forma um triângulo ou não
 
+def color(cor):
+    if cor == r1:
+        print('\033[91m')
+    elif cor == r2:
+        print('\033[92m')
+    elif cor == r3:
+        print('\033[93m')
+    else:
+        print('\033[0m')
+
+
 # Inserido os valores das 3 retas
-r1 = int(input('Informe o tamanho da reta A: '))
-r2 = int(input('Informe o tamanho da reta B: '))
-r3 = int(input('Informe o tamanho da reta C: '))
+r1 = int(input(f'Informe o tamanho da reta A: '))
+r2 = int(input(f'Informe o tamanho da reta B: '))
+r3 = int(input(f'Informe o tamanho da reta C: '))
+
 
 # Calculo para ser verdadeiro as condições de 3 retas formarem um triângulo
 # Condições para r1
 if (r2 - r3) < r1 < (r2 + r3):
-    print(f'\nA três retas forma um triângulo. \nPois a reta A({r1}) é maior que a Reta B({r2}) - Reta C({r3}) = ({r2 - r3}) e menor que a Reta B({r2}) + Reta C({r3}) = ({r2 + r3})')
+    print(f'\n{color(r1)}A três retas forma um triângulo. '
+          f'\nPois a reta A({(r1)}) é maior que a subtração da Reta B({r2}) - Reta C({r3}) = ({r2 - r3}) \n'
+          f'\nE menor que a soma da Reta B({r2}) + Reta C({r3}) = ({r2 + r3}){color("reset")}')
 
 # Condições para r2
 elif (r1 - r3) < r2 < (r1 + r3):
-    print(f'\nA três retas forma um triângulo. \nPois a reta B({r2}) é maior que a Reta A({r1}) - Reta C({r3}) = ({r1 - r3}) e menor que a Reta A{r1} + Reta C({r3}) = ({r1 + r3})')
+    print(f'\nA três retas forma um triângulo. '
+          f'\nPois a reta B({r2}) é maior que a subtração daReta A({r1}) - Reta C({r3}) = ({r1 - r3})'
+          f' \nE menor que a soma Reta A{r1} + Reta C({r3}) = ({r1 + r3})')
 
 # Condições para r3
 elif (r1 - r2) < r3 < (r1 + r2):
-    print(f'\nA três retas forma um triângulo. \nPois a reta C({r3}) é maior que a Reta A({r1}) - Reta B({r2}) = ({r1 - r2}) e menor que a Reta A({r1}) + Reta B({r2}) = ({r1 + r2})')
+    print(f'\nA três retas forma um triângulo. '
+          f'\nPois a reta C({r3}) é maior que a subtração da Reta A({r1}) - Reta B({r2}) = ({r1 - r2}) '
+          f'\nE menor que a soma da Reta A({r1}) + Reta B({r2}) = ({r1 + r2})')
 
 # Condições se caso as 3 retas não formarem um triângulo
 else:
     if r1 == r2 + r3 or r2 == r1 + r3 or r3 == r1 + r2:
-        print(f'\nAs retas não forma um triângulo. \nPois a maior reta é igual a soma das outras duas retas')
+        print(f'\nAs retas não forma um triângulo. '
+              f'\nPois a maior reta é igual a soma das outras duas retas')
     else:
-        print(f'\nAs retas não forma um triângulo. \nPois entre as três retas a soma de duas é maior')
+        print(f'\nAs retas não forma um triângulo. '
+              f'\nPois entre as três retas a soma de duas é maior')
