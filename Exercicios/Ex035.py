47f041bf7aa46e4b6b5a6fabab4b32b45a358eef
VictorSK20 <116979733+VictorSK20@users.noreply.github.com>
2023-10-06 15:47:54 -0400

    Update Ex035.py

--- Exercicios/Ex035.py
+++ Exercicios/Ex035.py
@@ -1,46 +1,40 @@
 # Crie um programa que leia o comprimento de 3 retas e informe se essas retas podem forma um triângulo ou não
 
+# Função para adiciona cores ao codigo
 def color(cor):
-    if cor == r1:
-        print('\033[91m')
-    elif cor == r2:
-        print('\033[92m')
-    elif cor == r3:
-        print('\033[93m')
+    if r1:
+        return '\033[93m'
+    elif r2:
+        return '\033[94m'
+    elif r3:
+        return '\033[95m'
     else:
-        print('\033[0m')
+        return '\033[0m'
 
 
-# Inserido os valores das 3 retas
-r1 = int(input(f'Informe o tamanho da reta A: '))
-r2 = int(input(f'Informe o tamanho da reta B: '))
-r3 = int(input(f'Informe o tamanho da reta C: '))
+try:
+    # Inserido os valores das 3 retas
+    r1 = int(input(f'Informe o tamanho da reta A: '))
+    r2 = int(input(f'Informe o tamanho da reta B: '))
+    r3 = int(input(f'Informe o tamanho da reta C: '))
 
+    # Calculo para ser verdadeiro as condições de 3 retas formarem um triângulo
+    # Condições para r1
+    if r1 > 0 and r2 > 0 and r3 > 0:
+        if r1 < (r2 + r3) or r2 < (r1 + r3) or r3 < (r1 + r2):
+            print(f'\n{color(r1)}As três retas formam um triângulo. '
+                  f'Pois a soma de qualquer lado é maior que o terceiro lado')
 
-# Calculo para ser verdadeiro as condições de 3 retas formarem um triângulo
-# Condições para r1
-if (r2 - r3) < r1 < (r2 + r3):
-    print(f'\n{color(r1)}A três retas forma um triângulo. '
-          f'\nPois a reta A({(r1)}) é maior que a subtração da Reta B({r2}) - Reta C({r3}) = ({r2 - r3}) \n'
-          f'\nE menor que a soma da Reta B({r2}) + Reta C({r3}) = ({r2 + r3}){color("reset")}')
-
-# Condições para r2
-elif (r1 - r3) < r2 < (r1 + r3):
-    print(f'\nA três retas forma um triângulo. '
-          f'\nPois a reta B({r2}) é maior que a subtração daReta A({r1}) - Reta C({r3}) = ({r1 - r3})'
-          f' \nE menor que a soma Reta A{r1} + Reta C({r3}) = ({r1 + r3})')
-
-# Condições para r3
-elif (r1 - r2) < r3 < (r1 + r2):
-    print(f'\nA três retas forma um triângulo. '
-          f'\nPois a reta C({r3}) é maior que a subtração da Reta A({r1}) - Reta B({r2}) = ({r1 - r2}) '
-          f'\nE menor que a soma da Reta A({r1}) + Reta B({r2}) = ({r1 + r2})')
-
-# Condições se caso as 3 retas não formarem um triângulo
-else:
-    if r1 == r2 + r3 or r2 == r1 + r3 or r3 == r1 + r2:
-        print(f'\nAs retas não forma um triângulo. '
-              f'\nPois a maior reta é igual a soma das outras duas retas')
+        # Condições se caso as 3 retas não formarem um triângulo
+        else:
+            if r1 == r2 + r3 or r2 == r1 + r3 or r3 == r1 + r2:
+                print(f'\nAs retas não forma um triângulo. '
+                      f'\nPois a maior reta é igual a soma das outras duas retas')
+            else:
+                print(f'\nAs retas não forma um triângulo. '
+                      f'\nPois entre as três retas a soma de duas é maior')
     else:
-        print(f'\nAs retas não forma um triângulo. '
-              f'\nPois entre as três retas a soma de duas é maior')
+        print('Favor digite um valor positivo')
+
+except ValueError:
+    print(f'\033[91mDigite um valor válido\033[m')
