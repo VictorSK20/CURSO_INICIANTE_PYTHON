19a7ffe0873c1f26dcc7226fbef7af285cae08ee
VictorSK20 <116979733+VictorSK20@users.noreply.github.com>
2023-10-16 10:50:34 -0400

    alterando Ex06, Ex07 e Ex08

--- Exercicios_Mundo1/Ex006.py
+++ Exercicios_Mundo1/Ex006.py
@@ -1,4 +1,15 @@
+<<<<<<< Updated upstream:Exercicios_Mundo1/Ex006.py
 print('\033[1;7;37mMostra o Doblo, Triplo e Raiz Quadrada do valor\033[0m\n')
 print('')
 n = int(input('Informe o Valor: \n'))
 print('\033[1mDoblo = \033[1;33m{}\033[0m \n\033[1mTriplo = \033[1;34m{}\033[0m \n\033[1mRaiz Quadrada = \033[1;35m{}\033[0m '.format(n*2, n*3, round(pow(n, 0.5))))
+=======
+print('\033[1;7;37mMostra o Doblo, Triplo e Raiz Quadrada do valor\033[0m\n')
+print('')
+
+n = int(input('Informe o Valor: \n'))
+
+print('\033[1mDoblo = \033[1;33m{}\033[0m \n'
+      '\033[1mTriplo = \033[1;34m{}\033[0m \n'
+      '\033[1mRaiz Quadrada = \033[1;35m{}\033[0m '.format(n*2, n*3, round(pow(n, 0.5))))
+>>>>>>> Stashed changes:Exercicios/Ex006.py
--- Exercicios_Mundo1/Ex007.py
+++ Exercicios_Mundo1/Ex007.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream:Exercicios_Mundo1/Ex007.py
 print('Mostra a nota do Aluno')
 print('')
 
@@ -16,3 +17,27 @@ nota2 = float(input('Informe a segunda nota do aluno: '))
 print(f'O aluno tem media ={media(nota1, nota2)}')
 
 
+=======
+print('Mostra a nota do Aluno')
+print('')
+
+# Criando a função "media" e passando como parametro ("nota1" e "nota2")
+def media(nota1, nota2):
+    media_aluno = (nota1 + nota2) / 2
+    if media_aluno < 8:
+        print(f'Aluno tirou media {media_aluno}\n\033[1;31mAluno reprovado\033[0m')
+    else:
+        print(f'Aluno tirou media {media_aluno}\n\033[1;32mAluno aprovado\033[0m')
+
+
+nota1 = float(input('Informe a primeira nota do aluno: '))
+if nota1 < 0 or nota1 > 10:
+    print('Nota inválida. As notas devem estar entre 0 e 10.')
+else:
+    nota2 = float(input('Informe a segunda nota do aluno: '))
+
+    if nota2 < 0 or nota2 > 10:
+        print('Nota inválida. As notas devem estar entre 0 e 10.')
+    else:
+        media(nota1, nota2)
+>>>>>>> Stashed changes:Exercicios/Ex007.py
