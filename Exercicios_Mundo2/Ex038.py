fee3d4582d4abf042754896657d54e665b835861
VictorSK20 <116979733+VictorSK20@users.noreply.github.com>
2023-10-30 15:43:00 -0400

    adicionando exercicios do 36 ao 39

---
+++ Exercicios_Mundo2/Ex038.py
@@ -0,0 +1,21 @@
+""" Escreva um programa que leia DOIS NÚMEROS inteiros e compare-os, mostrando
+uma mensagem na tela:
+O primeiro valor é maior
+O segundo valor é Maior
+Não existe valor maior, os dois são Iguais
+"""
+
+print('Informe Dois(2) números e veja qual deles é o maior')
+
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+print()
+
+if n1 > n2:
+    print(f'O primeiro valor é Maior\nresultado valor = {n1}')
+
+elif n2 > n1:
+    print(f'O segundo valor é Maior\nresultado valor = {n2}')
+
+elif n1 == n2:
+    print(f'Não existe valor maior, os dois valores são iguais\n primeiro valor = {n1} segundo valor = {n2}')
---
+++ Exercicios_Mundo2/Ex039.py
@@ -0,0 +1,18 @@
+"""Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com sua idade
+- Se ele AINDA VAI SE ALISTAR ao serviço militar.
+- Se é a HORA DE SE ALISTAR.
+- Se já PASSOU DO TEMPO do alistamento
+O programa também deverá mostrar o tempo que falta ou que passou do prazo"""
+
+import datetime
+
+print('Informe sua data de nascimento: ')
+
+dia = int(input('Informe o dia: '))
+mes = int(input('Informe o mês: '))
+ano = int(input('Informe o ano: '))
+
+data_nascimento = datetime.date(year=ano, month=mes, day=dia)
+
+
+# ano_nascimento = int(input('Infome sua data de nascimento'))
