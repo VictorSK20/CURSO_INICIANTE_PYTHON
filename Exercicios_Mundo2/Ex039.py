6f7e772539448ac97afc9cd4fa7d26fd2adb7d42
VictorSK20 <116979733+VictorSK20@users.noreply.github.com>
2023-11-13 15:32:53 -0400

    adicionando exercicios do 39 e 40

--- Exercicios_Mundo2/Ex039.py
+++ Exercicios_Mundo2/Ex039.py
@@ -1,9 +1,10 @@
-
-"""Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com sua idade
+"""
+Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com sua idade
 - Se ele AINDA VAI SE ALISTAR ao serviço militar.
 - Se é a HORA DE SE ALISTAR.
 - Se já PASSOU DO TEMPO do alistamento
-O programa também deverá mostrar o tempo que falta ou que passou do prazo"""
+O programa também deverá mostrar o tempo que falta ou que passou do prazo
+"""
 
 import datetime
 
@@ -13,12 +14,20 @@ dia = int(input('Informe o dia: '))
 mes = int(input('Informe o mês: '))
 ano = int(input('Informe o ano: '))
 
+# salvando dia, mês e ano da data de nascimento
 data_nascimento = datetime.datetime(year=ano, month=mes, day=dia)
 
-hoje = datetime.datetime.now()
+# salvando a idade do usúario utilizando o calculo da data atual menos a data de nascimento do usúario resultando no valor da idade
+idade = datetime.datetime.now().year - data_nascimento.year
 
-idade = hoje.year - data_nascimento.year
+if idade > 18:
+    print(f'Data de nascimento do jovem: {data_nascimento.strftime("%d/%m/%Y")}')
+    print(f'O jovem esta atrasado a {idade - 18} anos para o alistamento !')
 
-dias_alistamento =
+elif idade < 18:
+    print(f'Data de nascimento do jovem {data_nascimento.strftime("%d/%m/%Y")}')
+    print(f'Falta {18 - idade} anos para o jovem se alistar !')
 
-print(f'O jovem tem {idade}, e falta {dias_alistamento} dias para o alistamento militar')
+else:
+    print(f'Data de nascimento do jovem: {data_nascimento.strftime("%d/$m/%Y")}')
+    print(f'O jovem deve se alista neste ano')
---
+++ Exercicios_Mundo2/Ex040.py
@@ -0,0 +1,19 @@
+# Crie um programa que leia duas notas de um aluno e calcule sua média
+# Mostrando uma mensagem final, de acordo com a média atingida
+# abaixo de 5.0 = REPROVADO - entre 5.0 e 6.9 = RECUPERAÇÃO - igual ou superior a 7.0 = APROVADO
+print('Nota dos alunos:')
+
+# Primeira nota
+n1 = float(input('Informe a primeira nota do aluno: '))
+
+# Segunda nota
+n2 = float(input('Informe a segunda nota do aluno: '))
+
+media = (n1 + n2) / 2
+
+if media <= 5.0:
+    print(f'O aluno foi reprovado com: {media}')
+elif media >= 7.0:
+    print(f'O aluno foi aprovado com: {media}')
+else:
+    print(f'O aluno esta em recuperação com: {media}')
